ce37fc3856b910979befdef1ce5b31cff03714e4
Robbie Nhat Anh Ha Le <rha-le@c3a4c3.42berlin.de>
2025-03-21 17:18:27 +0100

    feat: server sends signal back for confirmation

--- src/client.c
+++ src/client.c
@@ -14,9 +14,9 @@
 #include <unistd.h>
 #include "libft.h"
 
-static void	send_bit(pid_t pid, unsigned char octet)
+static void	send_byte(pid_t pid, unsigned char octet)
 {
-	size_t i;
+	size_t	i;
 
 	i = 8;
 	while (i > 0)
@@ -25,23 +25,34 @@ static void	send_bit(pid_t pid, unsigned char octet)
 			kill(pid, SIGUSR2);
 		else
 			kill(pid, SIGUSR1);
-		usleep(50);
+		pause();
+		usleep(1);
 		--i;
 	}
 }
 
-static void	send_byte(pid_t pid, char *s)
+static void	send_str(pid_t pid, char *s)
 {
 	while (*s)
 	{
-		send_bit(pid, *s);
+		send_byte(pid, *s);
 		++s;
 	}
+	send_byte(pid, *s);
 }
 
-int main(int argc, char *argv[])
+static void	signal_handler(int sig)
 {
+	(void)sig;
+	if (sig == SIGUSR2)
+		ft_putstr_fd("All received!\n", 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	if (argc != 3)
 		return (1);
-	send_byte(ft_atoi(argv[1]), argv[2]);
+	send_str(ft_atoi(argv[1]), argv[2]);
 }
--- src/server.c
+++ src/server.c
@@ -25,27 +25,34 @@ static void	put_bit(int sig, unsigned char *g_c)
 		*g_c = *g_c << 1 | 1;
 }
 
-static void signal_handler(int sig)
+static void	signal_handler(int sig, siginfo_t *info, void *cntxt)
 {
-	static int		size = 0;
+	static int	size = 0;
 
+	(void)cntxt;
 	if (!size)
 		g_c = 0;
 	put_bit(sig, &g_c);
+	usleep(1);
+	kill(info->si_pid, SIGUSR1);
 	if (++size == 8)
 	{
-		ft_putchar_fd((char)g_c, 1);
+		if (g_c == 0)
+			kill(info->si_pid, SIGUSR2);
+		ft_putchar_fd(g_c, 1);
 		size = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
-	signal(SIGUSR1, signal_handler);
-	signal(SIGUSR2, signal_handler);
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	printf("PID: %i\n", getpid());
 	while (1)
-	{
 		pause();
-	}
 }
